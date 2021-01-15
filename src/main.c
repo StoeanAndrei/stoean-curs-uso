5116a8806c3424f48ddda0065dbd41b9d270484f
StoeanAndrei685 <74293009+StoeanAndrei@users.noreply.github.com>
2021-01-15 16:14:59 +0200

    update

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    int c = a+b;
+    printf("%d", c);
+    return 0;
+}
